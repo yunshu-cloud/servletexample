81e2c1bb0f9a3c2fd5e5b809358069255b7fc6b7
yunshu-cloud <w2099266050@gmail.com>
2023-08-07 18:54:54 +0800

    获取复选框的值

--- src/main/java/com/wangzhixiong/GetRequestDataServlet.java
+++ src/main/java/com/wangzhixiong/GetRequestDataServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class GetRequestDataServlet extends HttpServlet
 {
@@ -18,9 +20,16 @@ public class GetRequestDataServlet extends HttpServlet
         // 获取用户密码
         String userpwd =  req.getParameter("userpwd");
 
+        // 获取复选框的值
+        String[] hobby = req.getParameterValues("hobby");
+        List<String> list = Arrays.asList(hobby);
+
+
         PrintWriter pw = resp.getWriter();
         pw.println("UserName:"+username);
         pw.println("Password:"+userpwd);
+        pw.println("Hobby:"+list);
+
         pw.flush();
         pw.close();
     }
