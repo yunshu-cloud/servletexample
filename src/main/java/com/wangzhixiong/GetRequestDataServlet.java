133077893b686d11a67b0151bc9c4322c09817f6
yunshu-cloud <w2099266050@gmail.com>
2023-08-07 21:08:26 +0800

    获取所有提交数据的key

--- src/main/java/com/wangzhixiong/GetRequestDataServlet.java
+++ src/main/java/com/wangzhixiong/GetRequestDataServlet.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 
 public class GetRequestDataServlet extends HttpServlet
@@ -24,11 +26,19 @@ public class GetRequestDataServlet extends HttpServlet
         String[] hobby = req.getParameterValues("hobby");
         List<String> list = Arrays.asList(hobby);
 
+        // 获取所有提交谁的key
+        Enumeration<String> parameterNames = req.getParameterNames();
+        List<String> paraList = new ArrayList<>();
+        while(parameterNames.hasMoreElements())
+        {
+            paraList.add(parameterNames.nextElement());
+        }
 
         PrintWriter pw = resp.getWriter();
         pw.println("UserName:"+username);
         pw.println("Password:"+userpwd);
         pw.println("Hobby:"+list);
+        pw.println("Names:"+paraList);
 
         pw.flush();
         pw.close();
