a9d936cb2a5579a422193021a6dbad227f3c4b98
yunshu-cloud <w2099266050@gmail.com>
2023-08-09 21:58:18 +0800

    获取web.xml中的信息

---
+++ src/main/java/com/wangzhixiong/ContextReadInfoServlet.java
@@ -0,0 +1,42 @@
+package com.wangzhixiong;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * 读取web.xml配置文件中的<context-param>节点信息
+ */
+public class ContextReadInfoServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        // 获取ServletContext对象
+        ServletContext servletContext = this.getServletContext();
+
+        // 获取所有的context-param的值
+        Enumeration<String> initParameterNames = servletContext.getInitParameterNames();
+
+        PrintWriter pw = resp.getWriter();
+        while(initParameterNames.hasMoreElements())
+        {
+            String name = initParameterNames.nextElement();
+            String value = servletContext.getInitParameter(name);
+            pw.println("name:"+name+" = Value:"+value);
+        }
+        pw.flush();
+        pw.close();
+    }
+}
