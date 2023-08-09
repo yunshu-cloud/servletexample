6550e773a91a068777034d0fc8009d394ca7dd9e
yunshu-cloud <w2099266050@gmail.com>
2023-08-09 14:32:28 +0800

    获取请求头信息

--- src/main/java/com/wangzhixiong/GetRequestHeaderServlet.java
+++ src/main/java/com/wangzhixiong/GetRequestHeaderServlet.java
@@ -6,15 +6,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 public class GetRequestHeaderServlet extends HttpServlet
 {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+        // 根据指定的Key获取请求头信息
         String headerInfo = req.getHeader("Accept-Encoding");
+
+        // 获取请求头中的所有数据
+        Enumeration<String> headerNames = req.getHeaderNames();
+
+
         PrintWriter pw = resp.getWriter();
         pw.println("HeaderInfo - Accept-Encoding : "+headerInfo);
+        while(headerNames.hasMoreElements())
+        {
+            String key = headerNames.nextElement();
+            String value = req.getHeader(key);
+            pw.println(key+" : "+value);
+        }
         pw.flush();
         pw.close();
     }
