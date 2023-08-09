244857dd2cad764322cf8c823c519e98544e92f2
yunshu-cloud <w2099266050@gmail.com>
2023-08-09 17:31:58 +0800

    文件下载

---
+++ src/main/java/com/wangzhixiong/FileDownServlet.java
@@ -0,0 +1,40 @@
+package com.wangzhixiong;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * 文件下载
+ */
+public class FileDownServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        // 读取下载文件
+        File file = new File("/Users/wangzhixiong/Pictures/sakura.jpg");
+        InputStream is = new FileInputStream(file);
+        byte[] buff = new byte[is.available()];
+        is.read(buff);
+
+        // 在响应中添加附件信息
+        resp.addHeader("Content-Disposition","attachment;filename="+file.getName());
+
+        // 产生响应
+        OutputStream os = resp.getOutputStream();
+        os.write(buff);
+        os.flush();
+        os.close();
+
+
+    }
+}
